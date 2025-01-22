f648a8e47ee1308a4d4a79bb10fc8ce053ac7359
HarryY-Yao <[200~184984708+HarryY-Yao@users.noreply.github.com>
2025-01-22 11:21:46 -0500

    Create change size button

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const container = document.querySelector("#container");
+const button = document.querySelector(".btn");
 
 for (let i = 1; i <= 256; i++) {
     const grid = document.createElement("div");
 
     grid.classList.add("grid");
+    grid.style.cssText = "height: 50px; width: 50px;"
     grid.id = "" + i;
 
     container.appendChild(grid);
@@ -24,3 +26,44 @@ squares.forEach((square) => {
     });
 
 });
+
+let gridArray = Array.from(squares);
+
+let changeSize = (size) => {
+    let newHeight = 16 / size * 50 + ""
+
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+
+
+    for (let i = 1; i <= (size * size); i++) {
+        const grid = document.createElement("div");
+
+        grid.classList.add("grid");
+        grid.id = "" + i;
+        grid.style.cssText = `height: ${newHeight}px; width: ${newHeight}px;`
+
+        container.appendChild(grid);
+    }
+
+    const squares = document.querySelectorAll(".grid");
+
+    squares.forEach((square) => {
+
+    square.addEventListener("mouseover", () => {
+        square.style.backgroundColor = "orange";
+    });
+
+    square.addEventListener("mouseleave", () => {
+        setTimeout(() => {
+            square.style.backgroundColor = "";
+        }, 1000);
+    });
+
+});
+}
+
+button.addEventListener("click", () => {
+    changeSize(Number(prompt("Enter new size: ")));
+})
